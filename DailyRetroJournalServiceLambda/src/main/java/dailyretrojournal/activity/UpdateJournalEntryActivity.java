5275aede01205e8514feb715b4338e8c6bcef0b8
FrazierD <frazier.dyson@gmail.com>
2023-06-21 12:01:36 -0500

    refactored for deploying scenario4

--- DailyRetroJournalServiceLambda/src/main/java/dailyretrojournal/activity/UpdateJournalEntryActivity.java
+++ DailyRetroJournalServiceLambda/src/main/java/dailyretrojournal/activity/UpdateJournalEntryActivity.java
@@ -1,33 +1,33 @@
-package dailyretrojournal.activity;
-
-import dailyretrojournal.activity.request.UpdateJournalEntryRequest;
-import dailyretrojournal.converters.ModelConverter;
-import dailyretrojournal.dynamodb.JournalEntryDao;
-import dailyretrojournal.dynamodb.models.JournalEntry;
-import dailyretrojournal.models.JournalEntryModel;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import javax.inject.Inject;
-
-public class UpdateJournalEntryActivity {
-
-    private final Logger log = LogManager.getLogger();
-
-    private final JournalEntryDao journalEntryDao;
-
-    @Inject
-    public UpdateJournalEntryActivity(JournalEntryDao journalEntryDao) {
-        this.journalEntryDao = journalEntryDao;
-    }
-
-    public UpdateJournalEntryActivity handleRequest(final UpdateJournalEntryRequest updateEntryRequest){
-        String requestedEntryId = UpdateJournalEntryRequest.getEntryId();
-        JournalEntry journalEntry = journalEntryDao.updateJournalEntry(requestedEntryId);
-        JournalEntryModel journalEntryModel = new ModelConverter().toJournalEntryModel(journalEntry);
-    }
-
-
-
-
-}
+//package dailyretrojournal.activity;
+//
+//import dailyretrojournal.activity.request.UpdateJournalEntryRequest;
+//import dailyretrojournal.converters.ModelConverter;
+//import dailyretrojournal.dynamodb.JournalEntryDao;
+//import dailyretrojournal.dynamodb.models.JournalEntry;
+//import dailyretrojournal.models.JournalEntryModel;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
+//import javax.inject.Inject;
+//
+//public class UpdateJournalEntryActivity {
+//
+//    private final Logger log = LogManager.getLogger();
+//
+//    private final JournalEntryDao journalEntryDao;
+//
+//    @Inject
+//    public UpdateJournalEntryActivity(JournalEntryDao journalEntryDao) {
+//        this.journalEntryDao = journalEntryDao;
+//    }
+//
+//    public UpdateJournalEntryActivity handleRequest(final UpdateJournalEntryRequest updateEntryRequest){
+//        String requestedEntryId = UpdateJournalEntryRequest.getEntryId();
+//        JournalEntry journalEntry = journalEntryDao.updateJournalEntry(requestedEntryId);
+//        JournalEntryModel journalEntryModel = new ModelConverter().toJournalEntryModel(journalEntry);
+//    }
+//
+//
+//
+//
+//}
